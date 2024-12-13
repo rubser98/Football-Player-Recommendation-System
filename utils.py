d60c0083f7c3b11e1246bdee5366accc9e7bc05e
rubser98 <rubser17@gmail.com>
2024-12-13 10:56:09 +0100

    whoscored scraping, event2text

--- utils.py
+++ utils.py
@@ -4,6 +4,22 @@ def writeJson(dati, percorso_file):
     try:
         with open(percorso_file, 'w', encoding='utf-8') as file:
             json.dump(dati, file, ensure_ascii=False, indent=4)
-        #print(f"Dati scritti correttamente su {percorso_file}")
+
     except Exception as e:
         print(f"Errore durante la scrittura su file: {e}")
+
+def readJson(percorso_file):
+    """
+    Legge un file JSON e restituisce i dati come dizionario o lista.
+
+    :param percorso_file: Il percorso del file JSON.
+    :return: I dati contenuti nel file JSON.
+    """
+    try:
+        with open(percorso_file, 'r', encoding='utf-8') as file:
+            dati = json.load(file)
+        return dati
+    except FileNotFoundError:
+        print("Il file non è stato trovato.")
+    except json.JSONDecodeError:
+        print("Errore nel decodificare il file JSON.")
