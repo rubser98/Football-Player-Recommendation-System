71da787a0c9f0206dfea5425733994c16dcf89cc
rubser98 <rubser17@gmail.com>
2024-12-25 15:56:58 +0100

    aggiunta vocaboli al tokenizer

--- player2vec.py
+++ player2vec.py
@@ -2,6 +2,7 @@ import argparse
 from datasets import load_dataset
 from sentence_transformers import SentenceTransformer, losses
 import torch
+from utils import readJson
 
 if __name__ == "__main__":
     # Parse command line arguments
@@ -18,6 +19,16 @@ if __name__ == "__main__":
 
     # Initialize model
     model = SentenceTransformer("distiluse-base-multilingual-cased-v1", device="cuda" if torch.cuda.is_available() else "cpu")
+
+    vocab_dict = readJson('action_translations.json')
+    vocab = []
+    for v in vocab_dict.values():
+        for t in v.values():
+            tokens = t.split()
+            vocab = vocab + tokens
+    vocab = list(set(vocab))
+    model.tokenizer.add_tokens(vocab)
+    model.resize_token_embeddings(len(model.tokenizer))
 
     # Prepare training examples and DataLoader
     train_examples = dataset['train']['text']
