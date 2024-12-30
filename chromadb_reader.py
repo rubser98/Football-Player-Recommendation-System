afe48873ce24987f7a37d8a7340f8f096d51e291
rubser98 <rubser17@gmail.com>
2024-12-30 15:26:31 +0100

    average player embeddings su chroma db, prova descrizione giocatori

--- chromadb_reader.py
+++ chromadb_reader.py
@@ -1,5 +1,20 @@
 import chromadb
 import numpy as np
+import utils
+import os 
+from chromadb_ingestion import PlayerEmbeddingFunction
+from tqdm import tqdm
+
+def getPlayersList(path: str) -> set:
+    players_list = []
+    #for file in os.listdir(path):
+    for file in os.listdir(path):
+        dict_file = utils.readJson(f'{path}/{file}')
+        for row in dict_file.values():
+            players = list(row['players'].keys())
+            players_list = players_list + players
+    players_list = [float(player) if '.' in player else int(player) for player in players_list]
+    return set(players_list) - {0.0}
 
 if __name__ == '__main__':
     client = chromadb.PersistentClient(path="VectorDB")
@@ -7,14 +22,25 @@ if __name__ == '__main__':
     collection_name = "player_embeddings"  
     collection = client.get_collection(name=collection_name)
 
-    results = collection.get(where={
-            '$and': [
-                {'playerId': 349207.0},
-                {'season': '2021-2022'}
-            ]}, include=['embeddings'])
-    
-    print(type(results['embeddings']))
-    #print(results)
-    print(results.keys())
+    players_path = 'Dataset/Events2Text/TeamPlayerList'
+    players = getPlayersList(players_path)
 
-    print(results['embeddings'].mean(axis=0).shape)
+    avg_client = chromadb.PersistentClient(path="AVG_PLAYER_VECTORDB")
+    embedding_function = PlayerEmbeddingFunction(model_path="Model_v2/Model")
+
+    # Crea una collection usando la funzione di embedding personalizzata
+    avg_collection = client.get_or_create_collection(
+        name="average_player_embeddings",
+        embedding_function=embedding_function
+    )
+    with tqdm(total=len(players), desc="Processing players") as pbar:
+
+        for p in players:
+                pbar.update(1)
+                results = collection.get(where={'playerId': p}, include=['embeddings'])
+                avg_player_emb = results['embeddings'].mean(axis=0)
+                avg_collection.add(
+                    ids = [str(p)],
+                    embeddings=[avg_player_emb],
+                    metadatas={'playerId': p}
+                )
---
+++ player_description.py
@@ -0,0 +1,68 @@
+import chromadb
+import os
+import utils
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+
+def getPlayersDict(path: str) -> set:
+    players_list = {}
+    #for file in os.listdir(path):
+    for file in os.listdir(path):
+        dict_file = utils.readJson(f'{path}/{file}')
+        for row in dict_file.values():
+            players_list = players_list | row['players']
+            #players_list = players_list + players
+    #players_list = [float(player) if '.' in player else int(player) for player in players_list]
+    return players_list
+
+if __name__ == '__main__':
+
+    #vector store che contiene documenti ed embeddings di tutti i documenti (i.e. player-match)
+    client = chromadb.PersistentClient(path="VectorDB")
+    collection = client.get_collection(name="player_embeddings" )
+
+    avg_client = chromadb.PersistentClient(path="AVG_PLAYER_VECTORDB")
+    avg_collection = client.get_collection(name="average_player_embeddings")
+
+    p = 349207.0 #Rafa Leao
+    #p = 303115.0 #Theo
+    #p = 300713.0 #Mbappe
+    #p = 11119.0 #Messi
+    players_path = 'Dataset/Events2Text/TeamPlayerList'
+    players_dict = getPlayersDict(players_path)
+    player_name = players_dict[str(p)]
+
+    results = avg_collection.get(where={'playerId': p}, include=['embeddings'])['embeddings']
+
+    k = 10
+    top_k_docs = collection.query(query_embeddings= results, n_results=k)
+    doc = top_k_docs["documents"]
+
+    top_k_players = avg_collection.query(query_embeddings = results, n_results=k)
+    for kp in top_k_players['metadatas'][0]:
+        #print(kp, kp['playerId'])
+        print(players_dict[str(kp['playerId'])])
+
+
+    prompt = f"""[INST]
+    Sei un osservatore in ambito calcistico. Ho bisogno che mi crei un report per {player_name} evidenziando caratteristiche tecniche e tattiche, punti di forza e debolezze.
+    Ecco una lista di documenti che descrivono le azioni fatte durante le partite: 
+        {doc}
+
+    Restituisci il report nel seguente formato:
+
+    Giocatore : {player_name}
+    Caratteristiche:
+    Punti di forza:
+    Debolezze:
+    Zone del campo predilette:
+    [/INST]
+    """
+
+    model_name='rstless-research/DanteLLM-7B-Instruct-Italian-v0.1'
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_8bit=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids.cuda()
+    outputs = model.generate(input_ids=input_ids, max_new_tokens=200)
+    print(tokenizer.batch_decode(outputs, skip_special_tokens=True)[0].split("[/INST]")[1])
+
