62c473aac9072d18f8723ff2225e58f795c224b5
rubser98 <rubser17@gmail.com>
2024-12-30 16:13:51 +0100

    args chromadb reader

--- chromadb_reader.py
+++ chromadb_reader.py
@@ -4,6 +4,7 @@ import utils
 import os 
 from chromadb_ingestion import PlayerEmbeddingFunction
 from tqdm import tqdm
+import argparse
 
 def getPlayersList(path: str) -> set:
     players_list = []
@@ -17,16 +18,24 @@ def getPlayersList(path: str) -> set:
     return set(players_list) - {0.0}
 
 if __name__ == '__main__':
-    client = chromadb.PersistentClient(path="VectorDB")
+
+    parser = argparse.ArgumentParser(description="Store player embeddings into chroma db")
+    parser.add_argument("--vector_store_dir", type=str, required=True, help="Path to the directory containing the dataset file.")
+    parser.add_argument("--dataset_dir", type=str, required=True, help="Path to the directory containing the dataset file.")
+    parser.add_argument("--model_dir", type=str, required=True, help="Path to the directory where the model is stored.")
+    parser.add_argument("--output_dir", type=str, required=True, help="Path to the directory where the chromadb is stored.")
+    args = parser.parse_args()
+
+    client = chromadb.PersistentClient(path=args.vector_store_dir)
 
     collection_name = "player_embeddings"  
     collection = client.get_collection(name=collection_name)
 
-    players_path = 'Dataset/Events2Text/TeamPlayerList'
-    players = getPlayersList(players_path)
+    #players_path = 'Dataset/Events2Text/TeamPlayerList'
+    players = getPlayersList(args.dataset_dir)
 
-    avg_client = chromadb.PersistentClient(path="AVG_PLAYER_VECTORDB")
-    embedding_function = PlayerEmbeddingFunction(model_path="Model_v2/Model")
+    avg_client = chromadb.PersistentClient(path=args.output_dir)
+    embedding_function = PlayerEmbeddingFunction(model_path=args.model_dir)
 
     # Crea una collection usando la funzione di embedding personalizzata
     avg_collection = client.get_or_create_collection(
