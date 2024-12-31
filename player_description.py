16e2185f42bbb67f35884ce02d8f2c9b36370f20
rubser98 <rubser17@gmail.com>
2024-12-31 11:27:04 +0100

    llama3

--- player_description.py
+++ player_description.py
@@ -60,7 +60,8 @@ if __name__ == '__main__':
     [/INST]
     """
 
-    model_name='rstless-research/DanteLLM-7B-Instruct-Italian-v0.1'
+    #model_name='rstless-research/DanteLLM-7B-Instruct-Italian-v0.1'
+    model_name = 'meta-llama/Meta-Llama-3.1-8B-Instruct'
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_8bit=True)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
