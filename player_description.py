b8528605ae4542e8d8dcb327066804a5afbf3a73
rubser98 <rubser17@gmail.com>
2025-01-12 16:33:17 +0100

    llama3.2 3b

--- player_description.py
+++ player_description.py
@@ -118,8 +118,8 @@ if __name__ == '__main__':
     player_collection = client.get_collection(name="average_player_embeddings_version2")
     player_season_collection = client.get_collection(name="average_player_embeddings_season")
 
-    records = player_collection.get(limit=1)
-    print(records['metadatas'])
+    #records = player_collection.get(limit=1)
+    #print(records['metadatas'])
     p = 349207.0 #Rafa Leao
     #p = 303115.0 #Theo
     #p = 300713.0 #Mbappe
@@ -184,7 +184,7 @@ if __name__ == '__main__':
     
     #login()
     #model_name = 'meta-llama/Meta-Llama-3.1-8B-Instruct'
-    model_name = 'meta-llama/Meta-Llama-3.2-3B'
+    model_name = 'meta-llama/Llama-3.2-3B'
     #model_name = "galatolo/cerbero-7b"
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
