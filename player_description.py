91e59f3394c3c823c3c09f538338afb03b794b66
rubser98 <rubser17@gmail.com>
2025-01-12 16:27:24 +0100

    llama 3.2

--- player_description.py
+++ player_description.py
@@ -183,7 +183,8 @@ if __name__ == '__main__':
     #model_name='rstless-research/DanteLLM-7B-Instruct-Italian-v0.1'
     
     #login()
-    model_name = 'meta-llama/Meta-Llama-3.1-8B-Instruct'
+    #model_name = 'meta-llama/Meta-Llama-3.1-8B-Instruct'
+    model_name = 'meta-llama/Meta-Llama-3.2-3B'
     #model_name = "galatolo/cerbero-7b"
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
