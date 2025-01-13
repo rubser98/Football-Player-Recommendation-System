a4a85dc5b283cf36ac226230213674c02353ebc6
rubser98 <rubser17@gmail.com>
2025-01-13 21:50:11 +0100

    attention mask

--- player_description.py
+++ player_description.py
@@ -215,7 +215,7 @@ if __name__ == '__main__':
 
     with torch.no_grad():
         outputs = model.generate(input_ids=input_ids,
-                                 #attention_mask=attention_mask, 
+                                 attention_mask=attention_mask, 
                                  max_new_tokens=2000, 
                                  #pad_token_id=tokenizer.eos_token_id
                                  )
