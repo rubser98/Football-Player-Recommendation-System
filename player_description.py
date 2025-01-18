c3069e4892c124d5d44ccfe906954a749d93cd9d
rubser98 <rubser17@gmail.com>
2025-01-18 12:21:40 +0100

    split output test

--- player_description.py
+++ player_description.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
     print(len(outputs), outputs.shape)
     
     #print(tokenizer.batch_decode(outputs, skip_special_tokens=True)[0].split("[/INST]")[1])
-    generated_text = tokenizer.decode(outputs[0].split("###")[1], skip_special_tokens=True)
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True).split("###")[1]
     with open('prova_report_leao_qnt.txt', 'w') as f:
         f.write(generated_text)
 
