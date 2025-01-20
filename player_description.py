0e5b596703e50647ec1bb6720fa102da972c2b99
rubser98 <rubser17@gmail.com>
2025-01-20 12:18:06 +0100

    t aumentata

--- player_description.py
+++ player_description.py
@@ -232,7 +232,7 @@ if __name__ == '__main__':
                 outputs = model.generate(input_ids=input_ids,
                                         attention_mask=attention_mask, 
                                         max_new_tokens=2000, 
-                                        temperature=0.2,     # Modifica la temperatura qui
+                                        temperature=0.7,     # Modifica la temperatura qui
                                         top_k=50,            # Filtraggio top-k opzionale
                                         top_p=0.9,           # Nucleus sampling (top-p sampling) opzionale
                                         do_sample=True 
