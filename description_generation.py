4847842deb8be2fde2a675fd928488584e2ec12c
rubser98 <rubser17@gmail.com>
2025-01-24 09:56:37 +0100

    impostata liberazione memoria frammentata nella generazione e meccanismo di checkpoint e ripresa dal checkpoint nella generazione

--- description_generation.py
+++ description_generation.py
@@ -30,28 +30,41 @@ if __name__ == '__main__':
     if model.config.pad_token_id is None:
         model.config.pad_token_id = tokenizer.eos_token_id
     
+    count = 0
     with tqdm(total=len(prompt_dataset.keys()), desc="Processing players") as pbar:
 
         for p in prompt_dataset.keys():
             pbar.update(1)
-            prompt = prompt_dataset[p]['prompt']
-            input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids.cuda()
-            attention_mask = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True).attention_mask.cuda()
 
-            with torch.no_grad():
-                outputs = model.generate(input_ids=input_ids,
-                                        attention_mask=attention_mask, 
-                                        max_new_tokens=2000, 
-                                        temperature=0.7,     # Modifica la temperatura qui
-                                        top_k=20,            # Filtraggio top-k opzionale
-                                        top_p=0.8,           # Nucleus sampling (top-p sampling) opzionale
-                                        do_sample=True 
-                                        #pad_token_id=tokenizer.eos_token_id
-                                        )
+            #effettuo operazioni solo se non ho già generato descrizione per il giocatore nel caso di run multiple
+            if 'description' not in prompt_dataset[p].keys():
+                prompt = prompt_dataset[p]['prompt']
+                input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids.cuda()
+                attention_mask = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True).attention_mask.cuda()
+
+                with torch.no_grad():
+                    outputs = model.generate(input_ids=input_ids,
+                                            attention_mask=attention_mask, 
+                                            max_new_tokens=2000, 
+                                            temperature=0.7,     # Modifica la temperatura qui
+                                            top_k=20,            # Filtraggio top-k opzionale
+                                            top_p=0.8,           # Nucleus sampling (top-p sampling) opzionale
+                                            do_sample=True 
+                                            #pad_token_id=tokenizer.eos_token_id
+                                            )
+                
+                generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True).split("###Report generato:")[1]
+
+                prompt_dataset[p]['description'] = generated_text
+
+                del input_ids, attention_mask, outputs
+                torch.cuda.empty_cache()
+                count+=1
+
+            #ogni 10 descrizioni generate aggiorno il file target
+            if count % 10 == 0:
+                utils.writeJson(prompt_dataset, f'{args.dataset_dir}/player_description_{args.lang}_gen.json')
             
-            generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True).split("###Report generato:")[1]
-
-            prompt_dataset[p]['description'] = generated_text
 
     
     utils.writeJson(prompt_dataset, f'{args.dataset_dir}/player_description_{args.lang}_gen.json')
