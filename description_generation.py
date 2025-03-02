b87b7dc7b5c6243310263d71112567929c651d9f
rubser98 <rubser17@gmail.com>
2025-03-02 17:29:05 +0100

    llama quant

--- description_generation.py
+++ description_generation.py
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     model = AutoModelForCausalLM.from_pretrained(
             model_name,
             device_map="auto",
-            #load_in_8bit=True, 
-            #llm_int8_enable_fp32_cpu_offload=True,  # Solo se è su CPU
+            load_in_8bit=True, 
+            llm_int8_enable_fp32_cpu_offload=True,  # Solo se è su CPU
             offload_folder='offload_weights'  # Solo se è su CPU
             )
     
