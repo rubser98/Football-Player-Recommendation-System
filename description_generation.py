a876bf7a684a4bdc6c53472a95e8192fbb7cd2c1
rubser98 <rubser17@gmail.com>
2025-03-03 16:43:45 +0100

    prova qwen

--- description_generation.py
+++ description_generation.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if use_gpu else "cpu")
     #model_name = 'meta-llama/Llama-3.1-8B'
     #model_name = 'meta-llama/Llama-3.2-3B'
-    model_name = 'deepseek-ai/DeepSeek-R1-Distill-Qwen-14B'
+    #model_name = 'deepseek-ai/DeepSeek-R1-Distill-Qwen-14B'
     model_name = 'Qwen/Qwen2.5-7B-Instruct'
     model = AutoModelForCausalLM.from_pretrained(
             model_name,
