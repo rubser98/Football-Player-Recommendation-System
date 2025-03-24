aef35073ba01284422cd47a556427925b3eb9ea4
rubser98 <rubser17@gmail.com>
2025-03-24 17:49:42 +0100

    selezione acquisti

--- recommendation.py
+++ recommendation.py
@@ -64,8 +64,6 @@ class PlayerRecommendation:
             """
         )
         
-        print(self.llm)
-
         # Chain per generare raccomandazioni
         self.recommendation_chain = LLMChain(llm=self.llm, prompt=self.prompt_template)
         
