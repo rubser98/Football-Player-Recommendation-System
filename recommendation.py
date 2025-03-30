f9486c316b8df7c63e8773f4f8bd2fe5df21835c
rubser98 <rubser17@gmail.com>
2025-03-30 16:46:34 +0200

    exec su tutta la lista di trasferimenti

--- recommendation.py
+++ recommendation.py
@@ -326,7 +326,7 @@ class PlayerRecommendation:
     def main_recommendation(self, transfers_file):
 
         recommendations = []
-        transfers = readJson(f'{self.dir}/{transfers_file}')[:20]
+        transfers = readJson(f'{self.dir}/{transfers_file}')#[:20]
         
         with tqdm(total=len(transfers), desc="Processing recommendations") as pbar:   
             for t in transfers:
