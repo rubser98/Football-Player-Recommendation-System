0c7e302e8bee46a7bd30159d86ab1e330cd4e9c0
rubser98 <rubser17@gmail.com>
2025-03-30 13:56:03 +0200

    prova su 20 trasferimenti

--- recommendation.py
+++ recommendation.py
@@ -327,7 +327,7 @@ class PlayerRecommendation:
     def main_recommendation(self, transfers_file):
 
         recommendations = []
-        transfers = readJson(f'{self.dir}/{transfers_file}')[:3]
+        transfers = readJson(f'{self.dir}/{transfers_file}')[:20]
         
         with tqdm(total=len(transfers), desc="Processing recommendations") as pbar:   
             for t in transfers:
