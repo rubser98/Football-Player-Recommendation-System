954be87aa3c2a4914c058af5e4fd626e84423a2f
rubser98 <rubser17@gmail.com>
2025-04-02 16:56:33 +0200

    log

--- recommendation.py
+++ recommendation.py
@@ -381,9 +381,10 @@ class PlayerRecommendation:
             id = rec['id']
             ids_rec, ids_ret = self.get_ids_recommendation(rec)
             if id in ids_rec:
+                print('rec:', rec['id'], rec['player_name'])
                 hit_rec+=1
             if id in ids_ret:
-
+                print('ret:', rec['id'], rec['player_name'])
                 hit_ret+=1
             
             similarity_rec = self.verify_similarity_in_rec(id, ids_rec, threshold=0.2)
