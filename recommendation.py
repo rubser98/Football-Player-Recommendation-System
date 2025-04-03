539d618c5599e1cd95d15f6255becc6e8c935595
rubser98 <rubser17@gmail.com>
2025-04-03 22:38:10 +0200

    eval cf

--- recommendation.py
+++ recommendation.py
@@ -488,7 +488,7 @@ class PlayerRecommendation:
     def main_recommendation(self, transfers_file):
 
         recommendations = []
-        transfers = readJson(f'{self.dir}/{transfers_file}')[:20]
+        transfers = readJson(f'{self.dir}/{transfers_file}')
         
         with tqdm(total=len(transfers), desc="Processing recommendations") as pbar:   
             for t in transfers:
